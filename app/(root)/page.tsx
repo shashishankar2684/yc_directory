676797c919a57617cb613aeb4695552e2af1741f
shashi shankar <shashi.shankar2684@gmail.com>
2025-04-19 11:27:05 +0530

    Revert "sanity done"
    This reverts commit 3d6f7da5e756d2a7e771f25dff66f37e869a429e.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,10 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
-import StartupCard, { StartupTypeCard }from "@/components/StartupCard";
-import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home( { searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
-
-  // const posts = await client.fetch(STARTUPS_QUERY);
-
-  const { data : posts } = await sanityFetch({ query : STARTUPS_QUERY });
-
-
-  // const posts = [{
-  //   _createdAt: new Date(),
-  //   views: 55,
-  //   author: {_id: 1, name: 'shashi'},
-  //   _id : 1,
-  //   description: 'This is description',
-  //   image:"https://media.wired.com/photos/633b3fa691ba135542e3d5bd/master/w_2560%2Cc_limit/Tesla-Optimus-Bot-Event-Business.jpg",
-  //   category: 'robots',
-  //   title : 'we robots',
-  // }]
-
-
   return (
     <>
     <section className="pink_container">
@@ -39,25 +18,6 @@ export default async function Home( { searchParams }: {
     <SearchForm query={query} />
 
     </section>
-
-    <section className="section_container">
-      <p className="text-30-semibold">
-        {query ? `Search results for "${query}`: 'All Startups'}
-      </p>
-
-      <ul className="mt-7 card_grid">
-        {posts?.length > 0 ?(
-          posts.map((post: StartupTypeCard, index: number) => (
-            <StartupCard key={post?._id} post={post}/>
-          ))
-        ) : (
-          <p className="no-results">No startups found</p>
-        )}
-
-      </ul>
-    </section>
-
-    <SanityLive />
     
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import 'easymde/dist/easymde.min.css'
 
 const workSans = localFont({
   src: [
